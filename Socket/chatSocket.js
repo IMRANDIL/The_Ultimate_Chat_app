cf725f80f5518c6ac2204f06d1f372ad3dd943a7
IMRANDIL <aliimranadil2@gmail.com>
2023-07-02 07:00:53 +0530

    some more socket.io

--- Socket/chatSocket.js
+++ Socket/chatSocket.js
@@ -2,5 +2,10 @@ module.exports = (io) => {
   // console.log(io);
   io.on("connection", (socket) => {
     console.log(`connected to socket.io`);
+
+    socket.on("setup", (userData) => {
+      socket.join(userData._id); //created a particular room for this userId
+      socket.emit("connected");
+    });
   });
 };
